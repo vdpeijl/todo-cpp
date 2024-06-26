c73c4c295a3cd6212ea39734899203cda3452ae4
Robbin van der Peijl <robbin@sowiso.com>
2024-06-26 21:30:52 +0200

    Todo 'repl'

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ Todo* findTodoById(vector<Todo>& todos, const string id) {
   return nullptr;
 }
 
+void printTodos(const vector<Todo> todos) {
+  for (const Todo& todo : todos) {
+    string check = todo.getStatus() ? "x" : " ";
+    cout << "[" << check << "] " << todo.getId() << ". " << todo.getName() << endl;
+  }
+}
+
 int main() {
   vector<Todo> todos;
   Database db("todos.db", {"id", "name", "completed"});
@@ -21,8 +28,32 @@ int main() {
     todos.push_back(todo);
   }
 
-  Todo* found = findTodoById(todos, "1");
-  found->toggleStatus();
+  string input;
 
-  cout << "Name: " << found->getName() << endl;
+  while (true) {
+    cout << "##################################" << endl;
+    printTodos(todos);
+    cout << "##################################" << endl;
+    cout << "Enter command (or 'exit' to quit): " << endl;
+    getline(cin, input);
+
+    // Check if the user wants to exit
+    if (input == "exit" || input == "e") {
+      cout << "Exiting the program." << endl;
+      break;
+    }
+
+    if (input == "toggle" || input == "t") {
+      string id;
+      cout << "Toggle item by id: " << endl;
+      cin >> id;
+      Todo* found = findTodoById(todos, id);
+
+      if (found) {
+        found->toggleStatus();
+      } else {
+        cout << "Invalid todo id." << endl;
+      }
+    }
+  }
 }
